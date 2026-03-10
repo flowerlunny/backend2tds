1ec2064bfa2c27401346e094f0e561a981aad4b8
(˶˃ ᵕ ˂˶) <toledo.luna@escola.pr.gov.br>
2026-03-10 11:39:57 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço completo");
+let rua = "Rua Das Flores";
+let numero = 489;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade;
+//console.log(endereco);
+console.log("O endereço correto é: " + endereco);
