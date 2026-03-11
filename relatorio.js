b7108a583278ed3ca40173664cfee9897ca3b176
(˶˃ ᵕ ˂˶) <toledo.luna@escola.pr.gov.br>
2026-03-11 08:47:50 -0300

    Add files via upload

---
+++ relatorio.js
@@ -0,0 +1,9 @@
+//relatorio de notas
+let aluno = "Martin"; 
+let nota1 = 9.5;
+let nota2 = 4.9;
+
+let media = (nota1 + nota2) / 2;
+let relatorio = "Aluno: " + aluno + " | Notas: " + nota1 + " e " + nota2 + " | Média de notas: " + media;
+console.log("Relatório de Notas");
+console.log(relatorio);
